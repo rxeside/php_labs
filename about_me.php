fbb6f86cc10a2d4994ae0c113061b35dd8130eaf
rxeside <denis.leuhin@ispring.institute>
2024-02-12 18:20:34 +0300

    lab 1 without debug

---
+++ about_me.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Me</title>
+</head>
+<body>
+    <h1>About Me</h1>
+
+    <?php
+    $name = "Денис";
+    $surname = "Леухин";
+
+    $birth_date = new DateTime('2004-02-21');
+    $today = new DateTime();
+    $age = $today->diff($birth_date)->y;
+
+    $interests = array("Путешествия", "Игры", "Чтение", "Кино", "Спорт");
+
+    ?>
+
+    <h2>Имя: <?php echo htmlentities($name . " " . $surname); ?></h2>
+    <h2>Возраст: <?php echo htmlentities($age); ?></h2>
+
+    <h2>Интересы:</h2>
+    <ul>
+        <?php foreach ($interests as $interest) : ?>
+            <li><?php echo htmlentities($interest); ?></li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+</head>
+<body>
+    <h1>Welcome to My Website</h1>
+
+    <p>Check out <a href="about_me.php">about me</a> page.</p>
+</body>
+</html>
